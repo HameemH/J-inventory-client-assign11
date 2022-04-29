fcdc0bc50ff1a03356398464a011008fed740d57
HameemH <hameemhossain2@gmail.com>
2022-04-29 21:28:37 +0600

    navbar updated for conditional log out link

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import auth from './../../../firebse.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+  const[user] = useAuthState(auth);
+  const handleLogout = () =>{
+    signOut(auth);
+}
     return (
         <div>
             <Navbar className='background'  expand="lg">
@@ -12,7 +19,9 @@ const Header = () => {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link as={Link} to="login" className='text-white fw-bold'>Login</Nav.Link>
+       {
+         user? <Nav.Link className='text-white fw-bold' onClick={handleLogout}>Log out</Nav.Link>: <Nav.Link as={Link} to="login" className='text-white fw-bold'>Login</Nav.Link>
+       }
         <Nav.Link as={Link} to="registration" className='text-white fw-bold'>Registration</Nav.Link>
       
       </Nav>
